f6a4faf2ae49c6817e24077a4136cb767bca47e3
Eometheous <jonathan.sthomas@icloud.com>
2023-04-23 07:13:02 -0700

    Initial Commit

---
+++ Dining Philosopher Problem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Dining Philosopher Problem
+//
+//  Created by Jonathan Thomas on 4/23/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
